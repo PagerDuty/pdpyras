5f390dba0a88b7f89bb67862cefa51d619b98c21
Demitri Morgan <demitri@pagerduty.com>
2023-06-26 09:28:06 -0600

    Bump version

--- pdpyras.py
+++ pdpyras.py
@@ -18,7 +18,7 @@ from deprecation import deprecated, DeprecatedWarning
 from urllib3.exceptions import HTTPError, PoolError
 from requests.exceptions import RequestException
 
-__version__ = '5.0.4'
+__version__ = '5.1.0'
 
 #######################
 ### CLIENT DEFAULTS ###
